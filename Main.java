0d11c328fd1be0f48c880742d7b1fef1100a81a0
guillaume- <guillaume.ri.du@gmail.com>
2014-06-26 15:41:04 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import thread.SigThread;
+import data.DataStruc;
+
+public class Example2 {
+	public static void main(String[] args){
+		DataStruc d = new DataStruc(1, 1);
+		List<SigThread> l = new ArrayList<SigThread>();
+		for(int i=0; i<2; i++)
+			l.add(new SigThread(d, i));
+		for(SigThread t : l)
+			t.start();
+	}
+}
