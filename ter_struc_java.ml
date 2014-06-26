5ea720ae0ba70308dca7ac8bc2e401f2897b0218
guillaume- <guillaume.ri.du@gmail.com>
2014-06-26 17:01:42 +0200

    Update ter_struc_java.ml

--- ter_struc_java.ml
+++ ter_struc_java.ml
@@ -14,7 +14,8 @@ type variable = {
 	(* usage de la variable à t-1,
 	   ce qui implique la création d'une variable en t-1
 	   de même type, et dont le nom sera name^"1"
-	   S'il existe déjà une variable de même nom, elle sera renommée.
+	   il ne pourra pas exister de variable de même nom :
+	   toute variable doit avoir de base un nom = nomOrigine^"0"
 	*)
 }
 
@@ -25,17 +26,35 @@ type expression =
 	| Una_op of string*expression
 	| Call of string*(expression list)
 
+type const =
+	cV of variable
+	| cBin_op of const*string*const
+	| cUna_op of string*const
+
+type enum = {
+	e_name : string;
+	e_values : string list;
+}
+
 type thread = {
 	t_ins : variable list;
-	t_outs : variable list;
+	t_out : variable;
 	t_locals : variable list;
 	t_exp : expression;
-	t_const : const list;
+	t_const : (variable*string*const) list;
 }
+
+type procedure = {
+	p_in_types : string list;
+	p_out_type : string;
+}
+
 type java = {
 	j_ins : variable list;
 	j_outs : variable list;
 	j_locals : variable list;
 	j_constants : constant list;
 	j_threads : thread list;
+	j_enumT : enum list;
+	j_procedures : procedure list;
 }
