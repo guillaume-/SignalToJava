ec641f001cc48cbed7e1dbb2be8e019c2e47a8c9
guillaume- <guillaume.ri.du@gmail.com>
2014-06-27 09:37:13 +0200

    Update Signal.java

--- Signal.java
+++ Signal.java
@@ -1,19 +1,41 @@
 package data;
 
 public class Signal<Type> extends Event{
-	public Type t;
+	private Type t;
 	
+	public Signal(){
+		present = false;
+	}
+
 	public Signal(Type value){
 		t = value;
 		present = true;
 	}
-	
+
+	public Signal(Type value, boolean isPresent){
+		t = value;
+		present = isPresent;
+	}
+
 	public void setT(Type value){
 		t = value;
 		present = true;
 	}
+
+	public void setT(Signal<Type> s){
+		present = s.present;
+		if(present)
+			t = s.t;
+	}
 	
 	public void unsetT(){
 		present = false;
+	}
+	
+	public Type getT() throws UnpresentSignalException{
+		if(present)
+			return t;
+		else
+			throw new UnpresentSignalException();
 	}
 }
