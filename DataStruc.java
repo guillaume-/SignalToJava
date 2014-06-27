3b36f1930f74a7ff939cdea07b7daaac780093b5
guillaume- <guillaume.ri.du@gmail.com>
2014-06-27 09:11:20 +0200

    Update DataStruc.java

--- DataStruc.java
+++ DataStruc.java
@@ -1,12 +1,13 @@
 package data;
 
 public class DataStruc {
-	private int a0 = 1, b0 = 1, c0 = 0, d0 = 0; /* pour accéder à la valeur des variables (...à t-0) */
-	private final int constant1 = 1;
+	private Signal<Integer> a0 = new Signal<Integer>(1), b0 = new Signal<Integer>(1); /* pour accéder à la valeur des variables (...à t-0) */
+	private Signal<Integer> c0 = new Signal<Integer>(), d0 = new Signal<Integer>(); 
+	private final Signal<Integer> constant1 = new Signal<Integer>(1);
 	
 	public DataStruc(int a, int b){
-		a0 = a;
-		b0 = b;
+		a0.setT(a);
+		b0.setT(b);
 	}
 	
 	public void body(int n){
@@ -24,12 +25,12 @@ public class DataStruc {
 	public void uniq(int num){
 		switch(num){
 			case 0 :
-				c0 = a0;
-				System.out.printf("c0 = %d;\n", c0);
+				c0.setT(a0.t);
+				System.out.printf("c0 = %d;\n", c0.t);
 				break;
 			case 1 :
-				d0 = b0 + constant1;
-				System.out.printf("d0 = %d;\n", d0);
+				d0.setT(b0.t + constant1.t);
+				System.out.printf("d0 = %d;\n", d0.t);
 		}
 	}
 }
