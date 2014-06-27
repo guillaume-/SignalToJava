9fd1085cbd84335ffa6fdaf3e4dd152bb63ef85b
guillaume- <guillaume.ri.du@gmail.com>
2014-06-27 09:36:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,11 +7,11 @@ import thread.SigThread;
 import data.DataStruc;
 import usable.Usable;
 
-public class Example2 {
+public class Main {
 	public static void main(String[] args){
 		DataStruc d = new DataStruc(1, 1);
 		List<SigThread> l = new ArrayList<SigThread>();
-		for(int i=0; i<2; i++)
+		for(int i=0; i<3; i++)
 			l.add(new SigThread(d, i));
 		for(SigThread t : l)
 			t.start();
