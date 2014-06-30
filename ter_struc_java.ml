e43ac066f1fa8a9a4e55d183cc3207f9aa148ae8
guillaume- <guillaume.ri.du@gmail.com>
2014-06-30 16:45:38 +0200

    Update ter_struc_java.ml

--- ter_struc_java.ml
+++ ter_struc_java.ml
@@ -1,35 +1,28 @@
 (* type direction = Local | In | Out *)
 
-type constant = {
-	c_id : int; (* id unique *)
-	c_value : string;
-	c_type : string;
-}
-
 type variable = {
 	v_type : string;
 	v_name : string;
 (* 	v_usage : direction; pour faciliter le traitement plutot 3 listes différentes*)
-	v_isPassedUsed : bool;
+	v_valuesPassedUsed : string list;
 	(* usage de la variable à t-1,
-	   ce qui implique la création d'une variable en t-1
-	   de même type, et dont le nom sera name^"1"
-	   il ne pourra pas exister de variable de même nom :
-	   toute variable doit avoir de base un nom = nomOrigine^"0"
+	   ce qui implique la création de variables en t-1
+	   de même type, et dont les noms seront name^"num_used"^"1"
+	   toute variable ayant de base un nom = nomOrigine^"0",
+	   il ne pourra pas exister de variable de même nom
 	*)
 }
 
+type konstraint =
+	K_V of string
+	| K_Bin_op of konstraint*string*konstraint
+	| K_Una_op of string*konstraint
+
 type expression =
-	V of variable
-	| C of constant
+	V of string (* pour stocker constantes et variables de tout type *)
 	| Bin_op of expression*string*expression
 	| Una_op of string*expression
 	| Call of string*(expression list)
-
-type const =
-	cV of variable
-	| cBin_op of const*string*const
-	| cUna_op of string*const
 
 type enum = {
 	e_name : string;
@@ -37,24 +30,24 @@ type enum = {
 }
 
 type thread = {
-	t_ins : variable list;
-	t_out : variable;
-	t_locals : variable list;
-	t_exp : expression;
-	t_const : (variable*string*const) list;
+	t_variables : variable list; (* in, out, local *)
+	t_exp : (string*expression) list; (* plusieurs si cas séquentiel *)
+	t_const : (string*konstraint) list;
 }
 
 type procedure = {
 	p_in_types : string list;
+	p_name : string;
 	p_out_type : string;
 }
 
 type java = {
+	j_enumT : enum list; (* sauvegarde des types énumérés *)
+	j_procedures : procedure list; (* sauvegarde des procedure à prédéfinir dans Usable.java *)
 	j_ins : variable list;
 	j_outs : variable list;
 	j_locals : variable list;
-	j_constants : constant list;
 	j_threads : thread list;
-	j_enumT : enum list;
-	j_procedures : procedure list;
+	j_nbThreads : int;
+	(* souvent appelé : diminue la complexité plutot que List.length j_threads *)
 }
