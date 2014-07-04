a60c79f2c12f710882c973a8b28f3029aafd7137
guillaume- <guillaume.ri.du@gmail.com>
2014-07-04 13:40:24 +0200

    Update Signal.java

--- Signal.java
+++ Signal.java
@@ -1,8 +1,10 @@
 package data;
+import exceptions.UnpresentSignalException;
 
 public class Signal<Type> extends Event{
 	private Type t;
-	
+	private boolean loop_compute = false;
+
 	public Signal(){
 		present = false;
 	}
@@ -27,15 +29,31 @@ public class Signal<Type> extends Event{
 		if(present)
 			t = s.t;
 	}
-	
+
 	public void unsetT(){
 		present = false;
 	}
-	
+
 	public Type getT() throws UnpresentSignalException{
 		if(present)
 			return t;
 		else
 			throw new UnpresentSignalException();
+	}
+	
+	public Type t(){
+		return t;
+	}
+
+	public void new_loop(){
+		loop_compute = false;
+	}
+
+	public void now_computed(){
+		loop_compute = true;
+	}
+
+	public boolean isNotComputed(){
+		return !loop_compute;
 	}
 }
