f69ad1785669e1bc48067ac882b71d9912b713f2
guillaume- <guillaume.ri.du@gmail.com>
2014-07-08 16:23:08 +0200

    Update ter_struc_graph.ml

--- ter_struc_graph.ml
+++ ter_struc_graph.ml
@@ -6,22 +6,20 @@ We also can target assigns with their uniq output signal
 type in_port = {
 	in_name : string;
 	in_thread_used : int list; (* local signal if size == 1 *)
+	in_transi_links : out_port list;
 }
 and out_port = {
-	out_name : string;
-	out_thread_num : int; (* while not treated -1 *)
-	link : out_port option; (* if None end of a thread else compute the out_port *)
-	transitive_links : out_port list; (* *)
-	waited : bool; (* if true we'll have to notifyAll *)
+	o_name : string;
+	o_thread_num : int; (* while not treated -1 *)
+	o_link : out_port option; (* if None end of a thread else compute the out_port *)
+	o_transi_links : out_port list;
+	o_waited : bool; (* if true we'll have to notifyAll *)
 }
-and port = In of in_port | Out of out_port;;
 
-type path = {
+type thread = {
 	p_in : in_port list;
 	(* all In imply the first out_port, which imply the next outport, ... *)
 	p_path : out_port;
 }
 
-type graph = {
-	paths : path list; (* 2 paths <=> the input ports not counted, 2 lists without any recurrent port *)
-}
+type graph = thread list;; (* 2 paths <=> the input ports not counted, 2 lists without any recurrent port *)
