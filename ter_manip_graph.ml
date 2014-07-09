989d436952d6d3eb16ce0c3f1a6d597a3fe80522
guillaume- <guillaume.ri.du@gmail.com>
2014-07-09 16:55:48 +0200

    Update ter_manip_graph.ml

--- ter_manip_graph.ml
+++ ter_manip_graph.ml
@@ -1,16 +1,29 @@
 open Ter_struc_graph
+open Ter_struc_java
 
 let new_in_port name = {
 	in_name = name;
 	in_thread_used = [];
-	in_transi_links = [];
 }
 
-let new_out_port name = {
+let rec detect_ins ins =
+	let rec call_ins ins = function
+		[] -> []
+		|e::l -> (call_ins ins l)@(detect_ins ins e)
+	in function
+	V(s) -> if(List.exists (fun x -> x.in_name = s) ins)
+			then [(List.find (fun x -> x.in_name = s) ins)]
+			else []
+	| Bin_op(e1, s, e2) -> (detect_ins ins e1)@(detect_ins ins e2)
+	| Una_op(s, e) -> detect_ins ins e
+	| Call(s, eL) -> call_ins ins eL
+
+let new_out_port name expr ins = {
 	o_name = name;
-	o_thread_num = -1;
-	o_link = None;
-	o_transi_links = [];
+	o_ins = detect_ins ins expr;
+	o_outs = [];
+	o_next = None;
+	o_links = [];
 	o_waited = false;
 }
 
@@ -23,18 +36,18 @@ let is_fusion_needed t1 t2 = false
 Prend 2 chemins, retourne un seul chemin union des 2 tel que les ordres de définition de p1 et de p2 soient les plus optimisés
 *)
 let assigns_priority p1 p2 =
-	let is_possible p1 p2 = not (List.exists (fun x -> x=p1) p2.o_transi_links)
+	let is_possible p1 p2 = not (List.exists (fun x -> x=p1) p2.o_links)
 	in let rec tmp_res p1 p2 res = (*créé une liste de out dans l'ordre de définition souhaité*)
 		match(p2)with
 		Some(p) -> if(is_possible p1 p)
-					then tmp_res p p1.o_link (p1::res)
-					else tmp_res p1 p.o_link (p::res)
-		|None -> match(p1.o_link)with
+					then tmp_res p p1.o_next (p1::res)
+					else tmp_res p1 p.o_next (p::res)
+		|None -> match(p1.o_next)with
 					Some(x) -> tmp_res x None (p1::res)
 					|None -> res 
 	in let rec ordonner last_Value = function (*indique à tout out que son prochain est celui qui se trouve en i+1 dans la liste*)
 		[]-> last_Value
-		|e::l -> ordonner (Some({e with o_link = last_Value})) l
+		|e::l -> ordonner (Some({e with o_next = last_Value})) l
 	in let tmp = List.rev (tmp_res p1 (Some(p2)) [])
 	in match(ordonner None tmp)with
 		Some x -> x
@@ -42,11 +55,7 @@ let assigns_priority p1 p2 =
 (*
 Prend 2 threads, retourne un thread union des 2
 *)
-let fusion t1 t2 = 
-	let rec union ins = function
-		[] -> ins
-		|e::l -> if(List.exists (fun x -> x == e) ins)then (union ins l) else e::(union ins l)
-	in {
-	p_in = union t1.p_in t2.p_in;
+let fusion t1 t2 = {
+	p_id = t1.p_id;
 	p_path = assigns_priority t1.p_path t2.p_path;
 }
